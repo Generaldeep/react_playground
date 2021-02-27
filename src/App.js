d5af0ace60f41753971720456134d32e4934acd0
gurdipsingh <gurdipsingh1990@gmail.com>
2021-02-27 11:05:47 -0800

    cleaned up repo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
-     <h1>Test</h1>
+     <Header title="ello ello"/>
     </div>
   );
 }
