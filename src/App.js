7195af24bf01add4ccf5bea5cfde5f95e655e111
gurdipsingh <gurdipsingh1990@gmail.com>
2021-02-27 12:13:26 -0800

    cleaned up repo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Header from './components/Header';
+import Header from './components/Header/Header';
+import './App.css';
 
 function App() {
   return (
     <div className="app">
-     <Header title="ello ello"/>
+     <Header title="ello ello s"/>
     </div>
   );
 }
